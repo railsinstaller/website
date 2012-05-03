f3436bccccde40fb9edada9d87ba9a395975abd8
Gerlando Piro <gerlando@gmail.com>
2012-05-02 17:08:58 -0700

    initial addition of osx version

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,3 +6,17 @@ $('#video header a, #video a.video-thumbnail').click(function() {
 $('#lightbox').click(function() {
   $('#lightbox').hide();
 });
+
+$(function() {
+  if(navigator.platform.indexOf('Mac') != -1) {
+    $('#downloadlink').attr('href','https://github.com/downloads/railsinstaller/railsinstaller-nix/RailsInstaller-1.0.0-osx-10.7.app.tgz');
+    $('#windows').hide();
+    $('#osx').show();
+  }
+
+  if(navigator.platform.indexOf('Win') != -1) {
+  }
+
+  if(navigator.platform.indexOf('Linux') != -1) {
+  }
+});

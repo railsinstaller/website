7275e158d8e6681866aa44201bf395df7139b261
Evan Machnic <emachnic@broadmac.net>
2012-06-11 14:07:36 -0400

    Updated video for real this time

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -11,7 +11,7 @@ $(function() {
   if(navigator.platform.indexOf('Mac') != -1) {
     $('#downloadlink').attr('href','https://github.com/downloads/railsinstaller/railsinstaller-nix/RailsInstaller-1.0.0-osx-10.7.app.tgz');
     $('.vimeolink').attr('href','http://vimeo.com/43823464');
-    $('.vimeoframe').attr('src','http://player.vimeo.com/video/43220631?byline=0&amp;portrait=0');
+    $('.vimeoframe').attr('src','http://player.vimeo.com/video/43823464?byline=0&amp;portrait=0');
     $('#windows').hide();
     $('#osx').show();
   }

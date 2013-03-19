feaf21e6934a9c98eeb708b5ccbf1f5b03e3e64a
Evan Machnic <emachnic@broadmac.net>
2013-03-19 14:30:35 -0700

    Updated link for railsinstaller-win 2.2.1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def ri_win_download
-    "http://rubyforge.org/frs/download.php/76855/railsinstaller-2.2.0.exe"
+    "http://rubyforge.org/frs/download.php/76862/railsinstaller-2.2.1.exe"
   end
 end

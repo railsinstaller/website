eac1616b06a2fea4713d731bc296e6f321a0b81a
Kelvin Raffael Stinghen <kelvin.stinghen@gmail.com>
2013-06-26 16:35:12 -0300

    "#30 - Website Translation" - Refactor for translation - Including translation tags - Translated to Brazilian Portuguese

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || preferred_locale || I18n.default_locale
+  end
+
+  private
+    def available_languages
+      %w{en pt-BR} # TODO - find another way to get the language array
+    end
+
+    def preferred_locale
+      request.preferred_language_from(available_languages)
+    end
 end

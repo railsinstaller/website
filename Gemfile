7e87e07b83562ad6475334f70908da6843621821
Evan Machnic <emachnic@engineyard.com>
2013-07-12 12:10:56 -0400

    Revert "Merge branch 'translation_refactoring' of git://github.com/kelvinst/railsinstaller.org into kelvinst-translation_refactoring"
    This reverts commit 07f00c73c4752e02f01ad21554e1cd4ad87dab9b, reversing
    changes made to 7b8c281bd3531f9a5ede63f476d39221fc0e354e.

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
-gem 'rails', '~> 3.2.12'
-gem 'haml', '>= 3.0.0'
-gem 'haml-rails'
+gem "rails", "~> 3.2.12"
+gem "haml", ">= 3.0.0"
+gem "haml-rails"
 gem 'rake', '~> 10.0.3'
 gem 'puma', '~> 1.6.3'
 gem 'newrelic_rpm'
@@ -10,26 +10,23 @@ gem 'ey_config'
 gem 'honeybadger'
 
 group :assets do
-  gem 'sass-rails', '~> 3.2.5'
-  gem 'coffee-rails', '~> 3.2.2'
+  gem 'sass-rails', " ~> 3.2.5"
+  gem 'coffee-rails', " ~> 3.2.2"
   gem 'uglifier'
 end
 
-gem 'execjs'
 gem 'jquery-rails'
-gem 'http_accept_language'
 
 group :development do
-  gem 'gh_contributors'
+  gem "gh_contributors"
   gem 'hpricot'
-  gem 'therubyracer'
 end
 
 group :test do
-  gem 'cucumber-rails'
-  gem 'capybara'
-  gem 'ruby-debug19'
-  gem 'webrat'
-  gem 'rspec-rails', '~> 2.4'
-  gem 'omniauth', '~> 0.2.6'
+  gem "cucumber-rails"
+  gem "capybara"
+  gem "ruby-debug19"
+  gem "webrat"
+  gem "rspec-rails", "~> 2.4"
+  gem "omniauth", "~> 0.2.6"
 end
--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,10 @@
-require File.expand_path('../boot', __FILE__)
+require File.expand_path("../boot", __FILE__)
 
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'active_resource/railtie'
-require 'rails/test_unit/railtie'
-require 'sprockets/railtie'
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "rails/test_unit/railtie"
+require "sprockets/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -17,13 +17,14 @@ module RailsinstallerWeb
   class Application < Rails::Application
 
     config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
-    config.encoding = 'utf-8'
+    config.encoding = "utf-8"
     config.filter_parameters += [:password]
 
     # Enable the asset pipeline
     config.assets.enabled = true
-
+    
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
   end
 end

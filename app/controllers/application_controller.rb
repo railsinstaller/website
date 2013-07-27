33dbd5dc02a7af1378d16ce9d17ea7ff23ab650c
Yuriy Golobokov <yuriy.golobokov@icloud.com>
2013-07-27 13:51:52 +0600

    Fixed language detection and redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,42 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  AVAILABLE_LOCALES_HASH = {}
   before_filter :set_locale
 
   private
     def set_locale
-      I18n.locale = params[:locale] if params[:locale].present?
+      if params[:locale].present?
+        I18n.locale = params[:locale]
+      else
+        I18n.locale = extract_locale_from_header || I18n.default_locale
+        Rails.application.routes.default_url_options[:locale] = I18n.locale
+        redirect_to :root
+      end
+      Rails.application.routes.default_url_options[:locale] = I18n.locale
     end
 
-    def default_url_options options = {}
-      {locale: I18n.locale}
+    def extract_locale_from_header
+      return nil unless request.env['HTTP_ACCEPT_LANGUAGE']
+      begin
+        preferred_langs = request.env['HTTP_ACCEPT_LANGUAGE'].split(/,|;/).
+        grep(/^([a-z]{2}(-[a-z]{2})?)$/).map(&:to_sym).map(&:downcase)
+        match = preferred_langs & available_locales_hash.keys
+        if match.first
+          return available_locales_hash[match.first]
+        end
+      rescue
+        logger.error $!
+        return nil
+      end
+      return nil
     end
+
+    def available_locales_hash
+      return AVAILABLE_LOCALES_HASH if AVAILABLE_LOCALES_HASH.any?
+      I18n.available_locales.each do |locale|
+        AVAILABLE_LOCALES_HASH[locale.downcase] = locale
+      end
+      return AVAILABLE_LOCALES_HASH
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 RailsinstallerWeb::Application.routes.draw do
-  scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     root to: 'welcome#index'
   end
-
-  match '*path', to: redirect("/#{I18n.default_locale}/%{path}")
-  match '', to: redirect("/#{I18n.default_locale}")
 end
